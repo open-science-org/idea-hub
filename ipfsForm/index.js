7b604c91111e249598c5ce4fbea8f9cf46bb3b79
Gil-air-may <gilairmay@gmail.com>
2019-03-06 18:13:55 -0300

    Ran formatter on index.js

--- ipfsForm/index.js
+++ ipfsForm/index.js
@@ -1,78 +1,74 @@
-'use strict'
+"use strict";
 
 // this site has solution
 // https://github.com/ipfs/js-ipfs-api#cors
-var ipfsAPI = require('ipfs-api')
+var ipfsAPI = require("ipfs-api");
 
 // connect to ipfs daemon API server
-var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'}) // leaving out the arguments will default to these values
+var ipfs = ipfsAPI("localhost", "5001", { protocol: "http" }); // leaving out the arguments will default to these values
 
 // Listen for form submit
-document.getElementById("ipfsForm").addEventListener('submit', submitForm);
+document.getElementById("ipfsForm").addEventListener("submit", submitForm);
 
-function submitForm(e){
+function submitForm(e) {
   e.preventDefault();
 
   // Get values
-  var name = getInputVal('name');
-  var message = getInputVal('message');
+  var name = getInputVal("name");
+  var message = getInputVal("message");
 
   // save message
   saveMessage(message);
 
-  var linkMessage = getInputVal('linkMessage');
+  var linkMessage = getInputVal("linkMessage");
   linkMessages(message, linkMessage);
 }
 
 // function to get form values
-function getInputVal(id){
+function getInputVal(id) {
   return document.getElementById(id).value;
 }
 
-
 // Save message to firebase
-function saveMessage(message){
-
+function saveMessage(message) {
   var obj = {
     Data: message,
-    Links: [],
-  }
+    Links: []
+  };
 
   ipfs.object.put(obj, (err, result) => {
     if (err) {
-      throw err
+      throw err;
     }
 
-    document.getElementById('hash').value = result.toJSON().multihash
-
-  })
-
+    document.getElementById("hash").value = result.toJSON().multihash;
+  });
 }
 
-
 function linkMessages(message, linkMessage) {
-
   var obj = {
     Data: message,
-    Links: [],
-  }
+    Links: []
+  };
 
   ipfs.object.put(obj, (err, result) => {
     if (err) {
-      throw err
+      throw err;
     }
 
-    ipfs.object.patch.addLink(result, {
-      name: "new-link"
-    }, (err, res) => {
-      if (err) {
-        throw err
+    ipfs.object.patch.addLink(
+      result,
+      {
+        name: "new-link"
+      },
+      (err, res) => {
+        if (err) {
+          throw err;
+        }
+        console.log(res.toJSON());
       }
-      console.log(res.toJSON())
-    })
+    );
 
-    document.getElementById('hash').value = result.toJSON().multihash
-
-  })
-
+    document.getElementById("hash").value = result.toJSON().multihash;
+  });
 }
